841be7c6f8be89e23cadc8c644a2b3f373a70966
huzunrong <huzunrong@foxmail.com>
2024-04-18 23:54:22 +0800

    update 1.0.9

--- bean-invoker-intellij-plugin/src/main/java/io/github/newhoo/invoker/MyStartupActivity.java
+++ bean-invoker-intellij-plugin/src/main/java/io/github/newhoo/invoker/MyStartupActivity.java
@@ -1,9 +1,9 @@
 package io.github.newhoo.invoker;
 
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.ProjectActivity;
 import com.intellij.util.concurrency.AppExecutorUtil;
@@ -26,25 +26,39 @@ public class MyStartupActivity implements ProjectActivity {
     @Nullable
     @Override
     public Object execute(@NotNull Project project, @NotNull Continuation<? super Unit> continuation) {
-        ProgressManager.getInstance().run(new Task.Backgroundable(project, "Bean Invoker Check", false) {
+        ProgressManager.getInstance().run(new Task.Backgroundable(project, "Bean invoker check", false) {
             @Override
             public void run(@NotNull ProgressIndicator indicator) {
                 PluginProjectSetting setting = new PluginProjectSetting(project);
+                DumbService.getInstance(project)
+                        .runReadActionInSmartMode(()->{
+                            boolean springApp = AppUtils.isSpringApp(project);
+                            setting.setIsSpringApp(springApp);
 
-                ApplicationManager.getApplication().runReadAction(() -> {
-                    boolean springApp = AppUtils.isSpringApp(project);
-                    setting.setIsSpringApp(springApp);
-
-                    if (StringUtils.isNotEmpty(setting.getAgentPath()) && !new File(setting.getAgentPath()).exists()) {
-                        setting.setAgentPath(null);
-                    }
-                    if (springApp && StringUtils.isEmpty(setting.getAgentPath())) {
-                        AppExecutorUtil.getAppExecutorService().execute(() -> {
-                            AppUtils.getAgentPath("io.github.newhoo.bean-invoker", "bean-invoker-agent")
-                                    .ifPresent(setting::setAgentPath);
+                            if (StringUtils.isNotEmpty(setting.getAgentPath()) && !new File(setting.getAgentPath()).exists()) {
+                                setting.setAgentPath(null);
+                            }
+                            if (springApp && StringUtils.isEmpty(setting.getAgentPath())) {
+                                AppExecutorUtil.getAppExecutorService().execute(() -> {
+                                    AppUtils.getAgentPath("io.github.newhoo.bean-invoker", "bean-invoker-agent")
+                                            .ifPresent(setting::setAgentPath);
+                                });
+                            }
                         });
-                    }
-                });
+//                ApplicationManager.getApplication().runReadAction(() -> {
+//                    boolean springApp = AppUtils.isSpringApp(project);
+//                    setting.setIsSpringApp(springApp);
+//
+//                    if (StringUtils.isNotEmpty(setting.getAgentPath()) && !new File(setting.getAgentPath()).exists()) {
+//                        setting.setAgentPath(null);
+//                    }
+//                    if (springApp && StringUtils.isEmpty(setting.getAgentPath())) {
+//                        AppExecutorUtil.getAppExecutorService().execute(() -> {
+//                            AppUtils.getAgentPath("io.github.newhoo.bean-invoker", "bean-invoker-agent")
+//                                    .ifPresent(setting::setAgentPath);
+//                        });
+//                    }
+//                });
 
             }
         });
